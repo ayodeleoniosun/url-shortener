c2d35240ff3e1a14f79cfb9e5e76ebe7f5098618
Ayodele Oniosun <ayodele@ogunlabs.com>
2021-11-21 18:01:53 +0100

    chore: custom url validator

--- src/utils/helpers/url.utility.ts
+++ src/utils/helpers/url.utility.ts
@@ -1,12 +1,15 @@
-import validUrl from 'valid-url';
-
 export class UrlUtility {
   isValidUrl(url: string): boolean {
-    if (!url || !validUrl.isUri(url)) {
-      return false;
-    }
-
-    return true;
+    const pattern: RegExp = new RegExp(
+      '^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$',
+      'i'
+    );
+    return !!pattern.test(url);
   }
 
   generateShortCode(length: number): string {
