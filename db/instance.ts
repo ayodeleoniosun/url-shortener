79f1f6406f011150304110406e97d7c3af01380a
Ayodele Oniosun <ayodele@ogunlabs.com>
2021-11-23 08:17:07 +0100

    chore: remove port 3307

--- db/instance.ts
+++ db/instance.ts
@@ -3,7 +3,6 @@ import 'dotenv/config';
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  port: 3307,
   dialect: 'mysql',
 });
 
